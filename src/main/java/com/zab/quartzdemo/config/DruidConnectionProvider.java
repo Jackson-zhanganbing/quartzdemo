01f52ab828770f3a9dfc238d7c778f67e36d2399
张安兵 @0027017277 <635590867@qq.com>
2020-08-26 22:04:47 +0800

    字符集修改

--- src/main/java/com/zab/quartzdemo/config/DruidConnectionProvider.java
+++ src/main/java/com/zab/quartzdemo/config/DruidConnectionProvider.java
@@ -16,23 +16,37 @@ import java.sql.SQLException;
 public class DruidConnectionProvider implements ConnectionProvider {
     public static final int DEFAULT_DB_MAX_CONNECTIONS = 10;
     public static final int DEFAULT_DB_MAX_CACHED_STATEMENTS_PER_CONNECTION = 120;
-    //JDBC驱动
-    public String driver;
-    //JDBC连接串
-    public String URL;
-    //数据库用户名
-    public String user;
-    //数据库用户密码
-    public String password;
-    //数据库最大连接数
-    public int maxConnections;
-    //数据库SQL查询每次连接返回执行到连接池，以确保它仍然是有效的。
-    public String validationQuery;
+    /**
+     * JDBC驱动
+     */
+    private String driver;
+    /**
+     * JDBC连接串
+     */
+    private String URL;
+    /**
+     * 数据库用户名
+     */
+    private String user;
+    /**
+     * 数据库用户密码
+     */
+    private String password;
+    /**
+     * 数据库最大连接数
+     */
+    private int maxConnections;
+    /**
+     * 数据库SQL查询每次连接返回执行到连接池,以确保它仍然是有效的
+     */
+    private String validationQuery;
     public String maxCachedStatementsPerConnection;
     private boolean validateOnCheckout;
     private int idleConnectionValidationSeconds;
     private String discardIdleConnectionsSeconds;
-    //Druid连接池
+    /**
+     * Druid连接池
+     */
     private DruidDataSource datasource;
 
     public int getMaxConnections() {
